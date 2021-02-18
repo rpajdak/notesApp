c0d9f6bc5b3b4c381fd93eebc8338b1b773e5eb2
Rafal Pajdak <rpajdak@gmail.com>
2021-02-18 12:21:30 +0100

    Create Note model.

---
+++ src/main/java/rpajdak/demo/model/Note.java
@@ -0,0 +1,30 @@
+package rpajdak.demo.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+public class Note {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "created")
+    private LocalDate created;
+    @Column(name = "modified")
+    private LocalDate modified;
+
+    public Note() {
+    }
+}
