e3462a17c7a948834d2f1f788e0b12031e0cf665
Rafal Pajdak <rpajdak@gmail.com>
2021-02-18 12:25:30 +0100

    Add annotation @Repository.

--- src/main/java/rpajdak/demo/repository/NotesRepository.java
+++ src/main/java/rpajdak/demo/repository/NotesRepository.java
@@ -1,7 +1,9 @@
 package rpajdak.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rpajdak.demo.model.Note;
 
+@Repository
 public interface NotesRepository extends JpaRepository<Long, Note> {
 }
