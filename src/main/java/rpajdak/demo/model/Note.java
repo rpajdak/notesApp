ebdbf2f80e561e9db04c89c7ccc26c515fe063e7
Rafal Pajdak <rpajdak@gmail.com>
2021-02-19 18:46:12 +0100

    Add contructor with initilaId

--- src/main/java/rpajdak/demo/model/Note.java
+++ src/main/java/rpajdak/demo/model/Note.java
@@ -1,6 +1,8 @@
 package rpajdak.demo.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,28 +12,42 @@ import java.time.LocalDate;
 @Setter
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
 @Table(name = "note")
 public class Note {
 
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
     private Long id;
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "initial_id")
+    private Long initialId;
+
     @Column(name = "title")
     private String title;
+
     @Column(name = "content")
     private String content;
 
     @Column(name = "created")
     private LocalDate created;
+
     @Column(name = "modified")
     private LocalDate modified;
 
     public Note() {
     }
 
-
-    public Note(String title, String content) {
+    public Note(Long initialId, String title, String content, LocalDate created, LocalDate modified) {
+        this.initialId = initialId;
         this.title = title;
         this.content = content;
+        this.created = created;
+        this.modified = modified;
     }
+
+
+
 }
