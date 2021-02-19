8cba0df4be6af401144ed760bdd522cfc5027390
Rafal Pajdak <rpajdak@gmail.com>
2021-02-19 18:55:00 +0100

    Add method to get initialId of note.

--- src/main/java/rpajdak/demo/repository/NotesRepository.java
+++ src/main/java/rpajdak/demo/repository/NotesRepository.java
@@ -1,11 +1,15 @@
 package rpajdak.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rpajdak.demo.model.Note;
 
 @Repository
-public interface NotesRepository extends JpaRepository<Note,Long> {
+public interface NotesRepository extends JpaRepository<Note, Long> {
 
     Note getNoteById(Long id);
+
+    @Query("SELECT n.initialId FROM Note n WHERE n.id= :id")
+    Long getInitialId(Long id);
 }
